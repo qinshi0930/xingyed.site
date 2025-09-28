bceafee9d518ed3c436c3695b5d40582528e657f
qinshi0930 <music0930@163.com>
2025-09-28 23:20:00 +0800

    add logger utility for development

---
+++ src/lib/logger.ts
@@ -0,0 +1,35 @@
+type LogLevel = "log" | "error" | "warn" | "info";
+
+interface Logger {
+	log: (...args: any[]) => void;
+	error: (...args: any[]) => void;
+	warn: (...args: any[]) => void;
+	info: (...args: any[]) => void;
+}
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
+const logger: Logger = {
+	log: (...args: any[]) => {
+		if (isDevelopment) {
+			console.log(...args);
+		}
+	},
+	error: (...args: any[]) => {
+		if (isDevelopment) {
+			console.error(...args);
+		}
+	},
+	warn: (...args: any[]) => {
+		if (isDevelopment) {
+			console.warn(...args);
+		}
+	},
+	info: (...args: any[]) => {
+		if (isDevelopment) {
+			console.info(...args);
+		}
+	},
+};
+
+export default logger;
