faed7f8d152bcfce09cc5a11b65afaf7c2e9d9ab
qinshi0930 <music0930@163.com>
2025-09-28 10:53:03 +0800

    project initialized

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+const config = {
+	// singleQuote: true,
+	// jsxSingleQuote: true,
+	trailingComma: "all",
+	printWidth: 100,
+	proseWrap: "never",
+	endOfLine: "auto",
+	semi: true,
+	useTabs: true,
+	tabWidth: 4,
+	htmlWhitespaceSensitivity: "strict",
+};
+export default config;
