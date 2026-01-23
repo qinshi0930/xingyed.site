50077f5c6e5a394dc66ebd681e02d746dff807d9
qinshi0930 <music0930@163.com>
2026-01-23 17:34:38 +0800

    refactor: remove Prisma dependencies and configuration

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,20 +1,20 @@
-import "dotenv/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "prisma/config";
+// import "dotenv/config";
+// import path from "node:path";
+// import { fileURLToPath } from "node:url";
+// import { defineConfig } from "prisma/config";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export default defineConfig({
-	schema: path.join(__dirname, "src", "prisma", "schema"),
-	migrations: {
-		path: path.join(__dirname, "src", "prisma", "schema", "migrations"),
-		// seed: 'ts-node --compiler-options {"module":"CommonJS"} -r tsconfig-paths/register src/database/seed/index.ts',
-	},
-	views: {
-		path: path.join(__dirname, "src", "prisma", "schema", "views"),
-	},
-	typedSql: {
-		path: path.join(__dirname, "src", "prisma", "schema", "queries"),
-	},
-});
+// export default defineConfig({
+// 	schema: path.join(__dirname, "src", "prisma", "schema"),
+// 	migrations: {
+// 		path: path.join(__dirname, "src", "prisma", "schema", "migrations"),
+// 		// seed: 'ts-node --compiler-options {"module":"CommonJS"} -r tsconfig-paths/register src/database/seed/index.ts',
+// 	},
+// 	views: {
+// 		path: path.join(__dirname, "src", "prisma", "schema", "views"),
+// 	},
+// 	typedSql: {
+// 		path: path.join(__dirname, "src", "prisma", "schema", "queries"),
+// 	},
+// });
--- src/prisma/prisma.ts
+++ src/prisma/prisma.ts
@@ -1,22 +1,22 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+// const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString });
+// const adapter = new PrismaPg({ connectionString });
 
-const prisma = (() => {
-	if (process.env.NODE_ENV === "production") {
-		return new PrismaClient({ adapter });
-	} else {
-		const globalWithPrisma = globalThis as typeof globalThis & {
-			prisma: PrismaClient;
-		};
-		if (!globalWithPrisma.prisma) {
-			globalWithPrisma.prisma = new PrismaClient({ adapter });
-		}
-		return globalWithPrisma.prisma;
-	}
-})();
+// const prisma = (() => {
+// 	if (process.env.NODE_ENV === "production") {
+// 		return new PrismaClient({ adapter });
+// 	} else {
+// 		const globalWithPrisma = globalThis as typeof globalThis & {
+// 			prisma: PrismaClient;
+// 		};
+// 		if (!globalWithPrisma.prisma) {
+// 			globalWithPrisma.prisma = new PrismaClient({ adapter });
+// 		}
+// 		return globalWithPrisma.prisma;
+// 	}
+// })();
 
-export default prisma;
+// export default prisma;
